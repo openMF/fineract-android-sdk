f07958adf51f4004a35b83dd18d41e28ef9736d0
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-06-16 13:06:54 +0530

    changed Role to data class

--- core/src/main/java/org/mifos/core/models/client/Role.kt
+++ core/src/main/java/org/mifos/core/models/client/Role.kt
@@ -3,43 +3,12 @@ package org.mifos.core.models.client
 /**
  * Created by grandolf49 on 06-06-2020
  */
-class Role {
-    private var id = 0
-    private var name: String? = null
-    private var description: String? = null
-    private var disabled = false
-
-    fun getId(): Int {
-        return id
-    }
-
-    fun setId(id: Int) {
-        this.id = id
-    }
-
-    fun getName(): String? {
-        return name
-    }
-
-    fun setName(name: String?) {
-        this.name = name
-    }
-
-    fun getDescription(): String? {
-        return description
-    }
-
-    fun setDescription(description: String?) {
-        this.description = description
-    }
-
-    fun getDisabled(): Boolean {
-        return disabled
-    }
-
-    fun setDisabled(disabled: Boolean) {
-        this.disabled = disabled
-    }
+data class Role(
+    var id: Int = 0,
+    var name: String? = null,
+    var description: String? = null,
+    var disabled: Boolean = false
+) {
 
     override fun toString(): String {
         return "Role{" +
