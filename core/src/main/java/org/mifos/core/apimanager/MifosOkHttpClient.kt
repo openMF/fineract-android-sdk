aad13552cd7aca70d00be9c9ee7dc104aabf0d14
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-06-30 21:28:12 +0530

    updated MifosOkHttpClient.kt file with latest methods

--- core/src/main/java/org/mifos/core/apimanager/MifosOkHttpClient.kt
+++ core/src/main/java/org/mifos/core/apimanager/MifosOkHttpClient.kt
@@ -1,5 +1,6 @@
 package org.mifos.core.apimanager
 
+import android.annotation.SuppressLint
 import okhttp3.OkHttpClient
 import java.security.SecureRandom
 import java.security.cert.CertificateException
@@ -18,34 +19,26 @@ object MifosOkHttpClient {
     val mifosOkHttpClient: OkHttpClient
         get() = try {
             val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                @SuppressLint("TrustAllX509TrustManager")
                 @Throws(CertificateException::class)
-                override fun checkClientTrusted(
-                    chain: Array<X509Certificate>,
-                    authType: String
-                ) {
+                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
                 }
 
+                @SuppressLint("TrustAllX509TrustManager")
                 @Throws(CertificateException::class)
-                override fun checkServerTrusted(
-                    chain: Array<X509Certificate>,
-                    authType: String
-                ) {
+                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
                 }
 
                 override fun getAcceptedIssuers(): Array<X509Certificate> {
                     return arrayOf()
                 }
             })
-
-            // Install the all-trusting trust manager
             val sslContext = SSLContext.getInstance("SSL")
             sslContext.init(null, trustAllCerts, SecureRandom())
-            // Create an ssl socket factory with our all-trusting manager
-            val sslSocketFactory = sslContext.socketFactory
-            val builder = OkHttpClient.Builder()
-            builder.sslSocketFactory(sslSocketFactory)
-            builder.hostnameVerifier { hostname, session -> true }
-            builder.build()
+            OkHttpClient.Builder()
+                .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+                .hostnameVerifier { _, _ -> true }
+                .build()
         } catch (e: Exception) {
             throw RuntimeException(e)
         }
