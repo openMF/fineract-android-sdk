1ec37764a38efac8e48ff49df03abb9fec9ec5d7
Rajan Maurya <therajanmaurya@users.noreply.github.com>
2023-08-14 15:07:28 -0400

    Revert "Revert "Changed Base URL""

--- core/src/main/java/org/mifos/core/apimanager/BaseUrl.kt
+++ core/src/main/java/org/mifos/core/apimanager/BaseUrl.kt
@@ -8,7 +8,7 @@ package org.mifos.core.apimanager
 class BaseUrl {
     companion object {
         const val PROTOCOL_HTTPS = "https://"
-        const val API_ENDPOINT = "demo.mifos.io"
+        const val API_ENDPOINT = "dev.mifos.io"
         const val API_PATH = "/fineract-provider/api/v1/"
         const val PORT = "80"
     }
